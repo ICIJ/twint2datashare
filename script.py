7721e807f0962bdc462cd0f6f1c40de91cf0e725
Anne L'Hôte <anne.lhote@gmail.com>
2019-10-25 20:07:58 +0200

    Add CLI args : username, index, host, port

--- script.py
+++ script.py
@@ -12,6 +12,7 @@
 #
 # Libs
 #
+import click
 import datetime
 import elasticsearch
 from elasticsearch import helpers
@@ -24,13 +25,9 @@ import sys
 #
 # Config
 #
-user = sys.argv[1]
 log_file = 'twitter2datashare.log'
 log_level = logging.DEBUG
-input_file = user + '.json'
 output_file = 'requests'
-index = 'local-datashare'
-author = 'https://twitter.com/' + user + '/'
 path_project = '/home/dev/data'
 path_twitter = '/social_media/twitter'
 tweets_folder = 'tweets'
@@ -44,18 +41,16 @@ def get_current_time():
     s = t.strftime('%Y-%m-%dT%H:%M:%S.%f')
     return s[:-3] + 'Z'
 
-def delete_all_files():
-    for root, dirs, files in os.walk(tweets_folder):
-        for file in files:
-            os.remove(os.path.join(root, file))
-
-def delete_documents_from_elasticsearch(es):
-    es.delete_by_query(index=index,
-                       body='{"query": {"match": {"dirname": "' + path_project + path_twitter + '"}}}')
-
-def main():
-    es = elasticsearch.Elasticsearch()
-    delete_documents_from_elasticsearch(es)
+@click.command()
+@click.option("--username", prompt="Twitter username", help="Twitter username.")
+@click.option("--index", default='local-datashare', help="Elasticsearch index.")
+@click.option("--host", default='localhost', help="Elasticsearch host.")
+@click.option("--port", default='9200', help="Elasticsearch port.")
+def main(username, index, host, port):
+    input_file = username + '.json'
+    author = 'https://twitter.com/' + username + '/'
+    es = elasticsearch.Elasticsearch(hosts=[{'host': host, 'port': port}])
+    delete_documents_from_elasticsearch(es, index)
     delete_all_files()
     actions = []
     with open(input_file) as json_file:
@@ -72,7 +67,7 @@ def main():
                     "metadata": {
                         "tika_metadata_creation_date": tweet['date'] + 'T' + tweet['time'] + 'Z',
                         "tika_metadata_author": author,
-                        "tika_metadata_url": "https://twitter.com/" + user + "/status/" + str(tweet['id'])
+                        "tika_metadata_url": "https://twitter.com/" + username + "/status/" + str(tweet['id'])
                     },
                     "type": "Document",
                     "contentType": "application/json",
@@ -93,6 +88,15 @@ def main():
             file.write(json.dumps(tweet, indent=4))
             file.close()
     helpers.bulk(es, actions)
+
+def delete_all_files():
+    for root, dirs, files in os.walk(tweets_folder):
+        for file in files:
+            os.remove(os.path.join(root, file))
+
+def delete_documents_from_elasticsearch(es, index):
+    es.delete_by_query(index=index,
+                       body='{"query": {"match": {"dirname": "' + path_project + path_twitter + '"}}}')
 
 #
 # Main
