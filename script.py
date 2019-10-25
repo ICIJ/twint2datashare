7fed64e477a1a27a8914559418f6fe1131f1036a
Anne L'Hôte <anne.lhote@gmail.com>
2019-10-25 18:03:24 +0200

    Add bulk create in elasticsearch index

--- script.py
+++ script.py
@@ -17,6 +17,7 @@ import json
 import logging
 import os, os.path
 from elasticsearch import Elasticsearch
+from elasticsearch.helpers import bulk
 import sys
 
 
@@ -48,45 +49,50 @@ def delete_all_files():
         for file in files:
             os.remove(os.path.join(root, file))
 
-def delete_documents_from_elasticsearch():
-    es = Elasticsearch()
+def delete_documents_from_elasticsearch(es):
     es.delete_by_query(index=index,
                        body='{"query": {"match": {"dirname": "' + path_project + path_twitter + '"}}}')
 
 def main():
+    es = Elasticsearch()
+    delete_documents_from_elasticsearch(es)
     delete_all_files()
-    delete_documents_from_elasticsearch()
-    output = ''
+    actions = []
     with open(input_file) as json_file:
         tweets = json.load(json_file)
         for tweet in tweets:
             tweet_file = tweet['tweet'][:40].replace('/', '').replace('"', '') + '_' + str(tweet['id']) + '.json'
-            output += '{ "create" : { "_index" : "' + index + '", "_type" : "doc", "_id" : "'\
-                      + str(tweet['id']) + '" } }\n'
-            output += '{ ' \
-                    '"content" : "' + tweet['tweet'].replace('"', '\\"').replace('\r', '') + '",' \
-                    '"metadata" : {' \
-                        '"tika_metadata_creation_date" : "' + tweet['date'] + 'T' + tweet['time'] + 'Z' + '", ' \
-                        '"tika_metadata_author": "' + author + '", ' \
-                        '"tika_metadata_url": "https://twitter.com/' + user + '/status/' + str(tweet['id']) + '" ' \
-                    '}, ' \
-                    '"type" : "Document", ' \
-                    '"contentType" : "application/json", ' \
-                    '"language" : "ENGLISH", ' \
-                    '"extractionLevel" : 0, ' \
-                    '"dirname" : "' + path_project + path_twitter + '", ' \
-                    '"path" : "' + path_project + path_twitter + '/' + tweet_file + '", ' \
-                    '"extractionDate" : "' + get_current_time() + '", ' \
-                    '"status" : "INDEXED", ' \
-                    '"nerTags" : [], ' \
-                    '"join" : { "name" : "Document" } ' \
-                    '}\n'
+            object = {
+                "_op_type": "create",
+                "_index": index,
+                "_type": "doc",
+                "_id": str(tweet['id']),
+                "_source": {
+                    "content": tweet['tweet'].replace('"', '\\"').replace('\r', ''),
+                    "metadata": {
+                        "tika_metadata_creation_date": tweet['date'] + 'T' + tweet['time'] + 'Z',
+                        "tika_metadata_author": author,
+                        "tika_metadata_url": "https://twitter.com/" + user + "/status/" + str(tweet['id'])
+                    },
+                    "type": "Document",
+                    "contentType": "application/json",
+                    "language": "ENGLISH",
+                    "extractionLevel": 0,
+                    "dirname": path_project + path_twitter,
+                    "path": path_project + path_twitter + '/' + tweet_file,
+                    "extractionDate": get_current_time(),
+                    "status": "INDEXED",
+                    "nerTags": [],
+                    "join": {
+                        "name": "Document"
+                    }
+                }
+            }
+            actions.append(object)
             file = open(os.path.join(tweets_folder, tweet_file), 'w')
             file.write(json.dumps(tweet, indent=4))
             file.close()
-    file = open(output_file, 'w')
-    file.write(output)
-    file.close()
+    bulk(es, actions)
 
 #
 # Main
